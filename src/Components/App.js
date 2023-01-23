062ed129ac214120414a3756f306b4f5ef8217d0
dagger-v <goingarcher@gmail.com>
2023-01-23 02:45:10 -0800

    updated home page

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,13 +1,30 @@
 import Header from "./Header";
+import image from "../Images/vg-logo.png";
 
 import "./App.css";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <h1>test</h1>
-    </div>
+    <>
+      <div>
+        <Header />
+        <img className="logo" src={image} alt="logo"></img>
+        <div className="available-now">
+          <p>Summon Iconic Heroes</p>
+          <button>Available Now</button>
+        </div>
+      </div>
+      <div>
+        <h1 className="featured-text">Featured</h1>
+      </div>
+      <div className="featured-content">
+        <div className="featured"></div>
+        <div className="featured"></div>
+        <div className="featured"></div>
+        <div className="featured"></div>
+        <div className="featured"></div>
+      </div>
+    </>
   );
 }
 
