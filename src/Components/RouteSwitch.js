9bd1735ffe34b6c2f2c638df2c4f311906e8edb5
dagger-v <goingarcher@gmail.com>
2023-01-30 02:22:26 -0800

    updated paths

--- src/Components/RouteSwitch.js
+++ src/Components/RouteSwitch.js
@@ -7,7 +7,7 @@ const RouteSwitch = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/shopping-cart" element={<App />} />
         <Route path="/videogame" element={<VideoGame />} />
         <Route path="/shoppingcart" element={<ShoppingCart />} />
       </Routes>
